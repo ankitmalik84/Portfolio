78fa00fd7bd081833974a898fa82f9e687022b4a
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-01-11 07:11:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Badal_Singh
+This is my Portfolio Website, Which is made using HTML,CSS, and JS
+![portfolio](https://user-images.githubusercontent.com/76803084/160480465-b89ffd11-b826-4293-9192-932b2aede849.png)
---
+++ script.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  window.addEventListener("scroll", throttle(parallax, 14));
+
+  function throttle(fn, wait) {
+    var time = Date.now();
+    return function () {
+      if (time + wait - Date.now() < 0) {
+        fn();
+        time = Date.now();
+      }
+    };
+  }
+
+  function parallax() {
+    var scrolled = window.pageYOffset;
+    var parallax = document.querySelector(".parallax");
+    // You can adjust the 0.4 to change the speed
+    var coords = scrolled * 0.5 + "px";
+    parallax.style.transform = "translateY(" + coords + ")";
+  }
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Web Developer", "UX/UI Designer", "Coding Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
